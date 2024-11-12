d17b550a446e8a7091dfac31abbb943cc87e6f53
DCFApixels <99481254+DCFApixels@users.noreply.github.com>
2024-11-12 17:43:11 +0800

    Update Consts.cs

--- src/Consts.cs
+++ src/Consts.cs
@@ -1,11 +1,12 @@
 ﻿namespace DCFApixels.DragonECS.Recursivity
 {
+    using static EcsConsts;
     public static class EcsRecursivityConsts
     {
-        public const string PACK_GROUP = "_" + EcsConsts.FRAMEWORK_NAME + "/Recursivity";
+        public const string NAME_SPACE = AUTHOR + "." + FRAMEWORK_NAME + ".Recursivity.";
+        public const string PACK_GROUP = "_" + FRAMEWORK_NAME + "/Recursivity";
 
         public const int CRITICAL_RECURSIVE_COUNT = 100000;
-        public const string RECURSIVE_LAYER = nameof(RECURSIVE_LAYER);
-        public const string POST_RECURSIVE_LAYER = nameof(POST_RECURSIVE_LAYER);
+        public const string RECURSIVE_LAYER = NAME_SPACE + nameof(RECURSIVE_LAYER);
     }
 }
